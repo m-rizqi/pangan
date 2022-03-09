8a312ad83fdc8f020670b0c2ef576bdbf9e55798
Muhammad Rizqi <mrizqi070502@gmail.com>
2022-03-10 03:28:19 +0700

    setup font and color

--- app/src/main/java/com/satulima/pangan/MainActivity.kt
+++ app/src/main/java/com/satulima/pangan/MainActivity.kt
@@ -1,34 +1,18 @@
 package com.satulima.pangan
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.satulima.pangan.databinding.ActivityMainBinding
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
-    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen().apply {
-            setKeepVisibleCondition {
-                viewModel.isLoading.value
-            }
-        }
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
