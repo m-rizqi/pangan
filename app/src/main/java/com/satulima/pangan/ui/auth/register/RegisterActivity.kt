258dc65a1be6a83fbf90b29a90e6642c85dfdc56
Muhammad Rizqi <mrizqi070502@gmail.com>
2022-03-10 14:56:17 +0700

    account setup 1 done

--- app/src/main/java/com/satulima/pangan/ui/auth/register/RegisterActivity.kt
+++ app/src/main/java/com/satulima/pangan/ui/auth/register/RegisterActivity.kt
@@ -1,16 +1,30 @@
 package com.satulima.pangan.ui.auth.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.res.ResourcesCompat
 import com.satulima.pangan.R
+import com.satulima.pangan.databinding.ActivityRegisterBinding
+import com.satulima.pangan.ui.auth.setup.AccountSetupActivity
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         window.statusBarColor  = resources.getColor(R.color.yellow_light)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.buttonRegisterWithEmail.setOnClickListener {
+            startActivity(Intent(this, AccountSetupActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/satulima/pangan/ui/auth/setup/AccountSetupActivity.kt
+++ app/src/main/java/com/satulima/pangan/ui/auth/setup/AccountSetupActivity.kt
@@ -2,11 +2,23 @@ package com.satulima.pangan.ui.auth.setup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.satulima.pangan.R
+import com.satulima.pangan.databinding.ActivityAccountSetupBinding
 
 class AccountSetupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAccountSetupBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_account_setup)
+        binding = ActivityAccountSetupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.statusBarColor  = resources.getColor(R.color.grey_18)
+
+        navController = findNavController(R.id.navHostFragmentAccountSetup)
     }
 }
