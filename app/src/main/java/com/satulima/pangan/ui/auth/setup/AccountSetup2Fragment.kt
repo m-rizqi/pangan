25198fa55ecd174ea1f2d4e0643633fcc89c1f5e
Muhammad Rizqi <mrizqi070502@gmail.com>
2022-03-11 20:04:01 +0700

    birthday date picker

--- app/src/main/java/com/satulima/pangan/ui/auth/setup/AccountSetup2Fragment.kt
+++ app/src/main/java/com/satulima/pangan/ui/auth/setup/AccountSetup2Fragment.kt
@@ -1,24 +1,26 @@
 package com.satulima.pangan.ui.auth.setup
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.satulima.pangan.R
+import androidx.fragment.app.Fragment
+import com.satulima.pangan.databinding.FragmentAccountSetup2Binding
+import java.text.SimpleDateFormat
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AccountSetup2Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AccountSetup2Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
+    private var _binding : FragmentAccountSetup2Binding? = null
+    private val binding get() = _binding!!
+    val myCalendar: Calendar = Calendar.getInstance()
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -34,27 +36,45 @@ class AccountSetup2Fragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_setup2, container, false)
+        _binding = FragmentAccountSetup2Binding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AccountSetup2Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AccountSetup2Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onStart() {
+        super.onStart()
+        binding.editTextBirthday.setOnClickListener {
+            DatePickerDialog(
+                this.requireContext(),
+                provideDateListener(),
+                myCalendar[Calendar.YEAR],
+                myCalendar[Calendar.MONTH],
+                myCalendar[Calendar.DAY_OF_MONTH]
+            ).show()
+        }
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun provideDateListener(): OnDateSetListener {
+        val date =
+            OnDateSetListener { view, year, month, day ->
+                myCalendar[Calendar.YEAR] = year
+                myCalendar[Calendar.MONTH] = month
+                myCalendar[Calendar.DAY_OF_MONTH] = day
+                updateBirthdayText()
+            }
+        return date
+    }
+
+    private fun updateBirthdayText() {
+        val myFormat = "dd/MM/yyyy"
+        val dateFormat = SimpleDateFormat(myFormat, Locale.US)
+        binding.editTextBirthday.setText(dateFormat.format(myCalendar.time))
+    }
+
 }
